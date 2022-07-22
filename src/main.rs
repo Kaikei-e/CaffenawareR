4c2bac278d4f4a1b054a925edd033b01e235c957
Kaikei-e <e_kaikei@protonmail.com>
2022-07-22 22:57:31 +0900

    Update: cleaning up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use crate::api_handler::router;
 
-fn main() {
-    println!("Hello, world!");
+pub mod api_handler;
+
+fn main(){
+    router();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
